2e6894dd442bb450dfb21535aa6056de0dee74d9
Toheeb Olufade <olufade.toheeb@gmail.com>
2026-04-23 23:45:46 +0100

    feat: add auth API functions

---
+++ client/src/api/auth.ts
@@ -0,0 +1,11 @@
+import API from "./axios";
+import type { RegisterData, LoginData } from "../types/index";
+
+export const registerUser = (userData: RegisterData) => 
+    API.post('/auth/register', userData);
+
+export const loginUser = (loginData: LoginData) => 
+    API.post('/auth/login', loginData);
+
+export const verifyEmail = (token: string) =>
+    API.get(`/auth/verify-email/${token}`)
